f063b77f08b910d94486d0a5eedefdc521920961
KeyboardMedic <keyboardmedic@proton.me>
2024-05-13 03:04:03 +0200

    added bunch of extra logging aswell as a verbose setting and ability to ping users on errors in discord

--- clipbot.py
+++ clipbot.py
@@ -7,9 +7,13 @@ from subprocess import call
 from os.path import exists
 
 # formats embed for discord webhook and posts to url
-def discord_embed(title,color,description):
+def discord_embed(title,color,description,ping):
     if webhooklogurl != "":
-        data = {"embeds": [
+        if ping:
+            ping_string = f"<@{ping_id}>"
+        else:
+            ping_string = ""
+        data = {"content": ping_string, "embeds": [
                 {
                     "title": title,
                     "color": color,
@@ -17,23 +21,28 @@ def discord_embed(title,color,description):
                 }
             ]}
         rl = requests.post(webhooklogurl, json=data)
+        if verbose:
+            print(f"<CLIPBOT> sending message to discord webhook with title: {title} Color: {color} Description: {description} and ping: {ping_string}")
         time.sleep(1)
 
 # formats notification for use with gotify
 def gotify(title,message,priority):
     if gotifyurl != "":
         gr = requests.post(gotifyurl, data={"title": title, "message": message, "priority":priority})
-        print(f"sending notification to gotify with title: {title} message: {mesage} priority: {priority}")
+        if verbose:
+            print(f"<CLIPBOT> sending notification to gotify with title: {title} message: {message} priority: {priority}")
         time.sleep(1)
 
 # gets new auth token from twitch
 def gettoken():
-    print("Requesting new token from twitch")
-    discord_embed("Clipbot",14081792,"Requesting new token from twitch")
+    print("<CLIPBOT> Requesting new auth token from twitch")
+    discord_embed("Clipbot",14081792,"Requesting new auth token from twitch",False)
     rrr=requests.post("https://id.twitch.tv/oauth2/token", data={"client_id" : str(twitchClientId), "client_secret" : str(twitchSecret), "grant_type":"client_credentials"})
     tokenJson = rrr.json()
     token = tokenJson["access_token"]
-    print(f"new token is: {token}")
+    if verbose:
+        print(f"<CLIPBOT> new twitch auth token is: {token}")
+        discord_embed("Clipbot",14081792,"New twitch auth token recieved",False)
     with open(r'config/token.txt', 'w') as tokenFile:
         tokenFile.write("%s\n" % token)
         tokenFile.close()
@@ -44,8 +53,14 @@ def getstreamers():
     with open("config/streamers.txt", 'r') as streamerfile:
         streamers = [line.rstrip() for line in streamerfile]
         if "http" in streamers[0]:
+            if verbose:
+                print(f"found {str(streamers[0])} in streamers.txt and will follow it to get list of streamers")
+                discord_embed("Clipbot",14081792,f"found {str(streamers[0])} in streamers.txt and will follow it to get list of streamers",False)
             response = requests.get(streamers[0])
             streamers = response.text.splitlines()
+        if verbose:
+            print(f"<CLIPBOT> list of streamers to poll:\n{streamers}")
+            discord_embed("Clipbot",14081792,f"list of streamers to poll:\n{streamers}",False)
     return(streamers)
 
 # Loads variables used in script
@@ -56,18 +71,25 @@ with open("config/config.json") as config:
     webhookurl = configJson["webhookurl"]
     webhooklogurl = configJson["webhooklogurl"]
     webhookmonitorurl = configJson["webhookmonitorurl"]
+    webport = configJson["webport"]
     gotifyurl = configJson["gotifyurl"]
-    config.close()
+    ping_id = configJson["pingid"]
+    verbose = configJson["verbose"]
+    if verbose.lower() == "true":
+        verbose = True
+    elif verbose.lower() == "false":
+        verbose = False
 print("<CLIPBOT> succesfully loaded config")
-discord_embed("Clipbot",14081792,"succesfully loaded config")
+discord_embed("Clipbot",14081792,"succesfully loaded config",False)
 
 #webserver for monitoring purposes
-def thread_second(): # start webserver.py as a second threat to allow it to run parallel with main script
-    call(["python", "webserver.py"])
-processThread = threading.Thread(target=thread_second)
-processThread.start()
-print("<CLIPBOT> starting webserver for local monitoring") 
-discord_embed("Clipbot",14081792,"starting webserver for local monitoring")
+if webport != "":
+    def thread_second(): # start webserver.py as a second threat to allow it to run parallel with main script
+        call(["python", "webserver.py"])
+    processThread = threading.Thread(target=thread_second)
+    processThread.start()
+    print("<CLIPBOT> starting webserver for local monitoring") 
+    discord_embed("Clipbot",14081792,"starting webserver for local monitoring",False)
 
 #post process to talk to remote monitor
 if webhookmonitorurl != "":
@@ -76,7 +98,7 @@ if webhookmonitorurl != "":
     processThread = threading.Thread(target=thread_third)
     processThread.start()
     print("<CLIPBOT> starting post server for remote monitoring")
-    discord_embed("Clipbot",14081792,"starting post server for remote monitoring")
+    discord_embed("Clipbot",14081792,"starting post server for remote monitoring",False)
 
 #opens file to get auth token
 token_exists = exists("config/token.txt")
@@ -85,8 +107,8 @@ if token_exists == True:
         tokenRaw = str(file2.readline())
         token = tokenRaw.strip()
         file2.close()
-    print ("Token to use for auth: " + token)
-    discord_embed("Clipbot",14081792,"auth token loaded succesfully")
+    print ("<CLIPBOT> Twitch auth oken to use for auth: " + token)
+    discord_embed("Clipbot",14081792,"Twitch auth token loaded succesfully",False)
 else:
     token = gettoken()
 
@@ -96,16 +118,27 @@ while True:
     # reads streamers.txt
         streamers = getstreamers()
     # opens clips file and loads it for comparison later
+        if verbose:
+            print("<CLIPBOT> checking if clips.txt exsists")
+            discord_embed("Clipbot",14081792,"checking if clips.txt exsists",False)
         clips_exists = exists("config/clips.txt")
         if clips_exists == True:
             with open("config/clips.txt", 'r') as clipsFile: 
                 clips = [line.rstrip() for line in clipsFile]
+            if verbose:
+                print(f"<CLIPBOT> content of clips.txt is:\n{clips}")
+                discord_embed("Clipbot",14081792,f"content of clips.txt is:\n{clips}",False)
             with open("config/clips.txt",'w') as clipsFile:
                 pass
+            if verbose:
+                print("<CLIPBOT> clips.txt read and erased")
+                discord_embed("Clipbot",14081792,"clips.txt read and erased",False)
         else:
             clips = []
             with open("config/clips.txt",'w') as clipsFile:
                 pass
+            print("<CLIPBOT> clips.txt did not exsist and was created")
+            discord_embed("Clipbot",14081792,"clips.txt did not exsist and was created",False)    
     # Gets current time and hour ago in utc for use in request
         currentTime = (datetime.now(timezone.utc))
         currentTimeFormatted = (currentTime.strftime("%Y-%m-%dT%H:%M:%SZ"))
@@ -115,46 +148,49 @@ while True:
         for streamer in streamers:
             try:
                 r=requests.get(f"https://api.twitch.tv/helix/users?login={streamer}", headers={'Authorization':f"Bearer {token}", 'Client-Id':twitchClientId})
-                print(f"Request response: {r}")
+                print(f"<CLIPBOT> Request response: {r}")
         # checks if token is valid and requests a new one if not
                 if "401" in str(r):
                     token = gettoken()
                     r=requests.get(f"https://api.twitch.tv/helix/users?login={streamer}", headers={'Authorization':f"Bearer {token}", 'Client-Id':twitchClientId})
-                elif "200" in str(r):
-                    print(f"{token} is valid and will be used")
+                elif "200" in str(r) and verbose:
+                    print(f"<CLIPBOT> twitch auth token: {token} is valid and will be used")
                 else:
-                    discord_embed("Clipbot",10159108,f"request response: {r}")
+                    discord_embed("Clipbot",10159108,f"request response: {r}",False)
         # Continues pulling clips from twitch api
                 streamerJson = r.json()
                 streamerId = streamerJson["data"][0]["id"]
                 print(f"<CLIPBOT> streamer used to poll for clips {streamer} {streamerId}")
-                discord_embed("Clipbot",14081792,f"streamer used to poll for clips {streamer} {streamerId}")
+                discord_embed("Clipbot",14081792,f"streamer used to poll for clips {streamer} {streamerId}",False)
                 rr=requests.get(f"https://api.twitch.tv/helix/clips?broadcaster_id={streamerId}&started_at={hourAgoFormatted}", headers={'Authorization':f"Bearer {token}", 'Client-Id':twitchClientId})
                 clipsJson = rr.json()
-                print (f"json recieved from poll: {json.dumps(clipsJson)}")
+                if verbose:
+                    print (f"<CLIPBOT> json recieved from poll: {json.dumps(clipsJson)}")
         # loop start
                 for clip in clipsJson["data"]:
                     clipUrl = clip["url"]
-                    print(f"url to post {clipUrl}")
+                    if verbose:
+                        print(f"<CLIPBOT> url to post {clipUrl}")
+                        discord_embed("Clipbot",14081792,f"url to post {clipUrl}",False)
         # checks if clip was allready posted
                     if clipUrl not in clips:
                         r = requests.post(webhookurl, data={"content": clipUrl})
-                        print(f"{clipUrl} posted on discord")
-                        discord_embed("Clipbot",703235,f"{clipUrl} posted on discord")
+                        print(f"<CLIPBOT> {clipUrl} posted on discord")
+                        discord_embed("Clipbot",703235,f"{clipUrl} posted on discord",False)
                         with open('config/clips.txt', 'a') as clipsFile:
                             clipsFile.write(f"{clipUrl}\n")
-                    else:
-                        print(f"{clipUrl} not posted because it was allready posted")
-                        discord_embed("Clipbot",14081792,f"{clipUrl} not posted because it was allready posted")
+                    elif verbose:
+                        print(f"<CLIPBOT> {clipUrl} not posted because it was allready posted")
+                        discord_embed("Clipbot",14081792,f"{clipUrl} not posted because it was allready posted",False)
             except Exception as e:
-                print(f"An exception occurred in the main loop whilst checking streamer: {streamer} {str(e)}")
-                discord_embed("Clipbot",10159108,f"An exception occurred in the main loop whilst checking streamer: {streamer} {str(e)}")
+                print(f"<CLIPBOT> An exception occurred in the main loop whilst checking streamer: {streamer} {str(e)}")
+                discord_embed("Clipbot",10159108,f"An exception occurred in the main loop whilst checking streamer: {streamer} {str(e)}",True)
                 gotify("Clipbot",f"An exception occurred in the main loop whilst checking streamer: {streamer} {str(e)}","5")
-        print("waiting for 1 hour")
-        discord_embed("Clipbot",14081792,"waiting for 1 hour")
+        print("<CLIPBOT> waiting for 1 hour")
+        discord_embed("Clipbot",14081792,"waiting for 1 hour",False)
         time.sleep(3600)
     except Exception as e:
-        print(f"An exception occurred in the main loop: {str(e)} waiting for 1 minute")
-        discord_embed("Clipbot",10159108,f"An exception occurred in the main loop: {str(e)} waiting for 1 minute")
+        print(f"<CLIPBOT> An exception occurred in the main loop: {str(e)} waiting for 1 minute")
+        discord_embed("Clipbot",10159108,f"An exception occurred in the main loop: {str(e)} waiting for 1 minute",True)
         gotify("Clipbot",f"An exception occurred in the main loop: {str(e)} waiting for 1 minute","5")
         time.sleep(60)
--- readme.md
+++ readme.md
@@ -3,8 +3,8 @@ A simple bot that polls twitch for clips from a predefined list of streamers ove
 
 included are:
 - a post script that sends a http post to a watchdog server of your choosing, wich wont happen if the url is left blank in the config
-- a http webserver running to use for uptime monitoring by having an approachable url wich can be checked with something like uptime kuma
-- remote logging capabilities with use of a discord webhook where debugging messages get posted, wich wont happen if the url is left blank in the config
+- a http webserver running to use for uptime monitoring by having an approachable url wich can be checked with something like uptime kuma, NOTE must be integer, Can be left blank to disable.
+- remote logging capabilities with use of a discord webhook where debugging messages get posted, wich wont happen if the url is left blank in the config. Additionally a user or group id can be defined in the config so that group or user will be pinged if an error occurs, can also be left blank to disable
 - remote alerts on errors trough gotify, wich wont happen if the url is left blank in the config
 
 # how to use:
@@ -19,10 +19,11 @@ included are:
     "webhooklogurl": "WEBHOOK_TO_USE_TO_LOG_DEBUGGING_MESSAGES_TO_A_WEBHOOK/OPTIONAL",
     "webhookmonitorurl": "URL_FOR_REMOTE_MONITORING_SERVER/OPTIONAL",
     "gotifyurl": "URL_FOR_ALERTS_ON_ERRORS_TROUGH_GOTIFY/OPTIONAL",
+    "pingid": "USER OR GROUP ID TO PING WHEN AN ERROR OCCURS/OPTIONAL",
     "botname": "NAME_VALUE_SEND_TO_REMOTE_MONITORING_SERVER/OPTIONAL",
     "posttimeout": "TIME_BETWEEN_POSTS_TO_REMOTE_MONITORING/OPTIONAL",
     "hostname": "ADRESS_FOR_LOCAL_WEBSERVER",
-    "webport": PORT_FOR_LOCAL_WEBSERVER
+    "webport": PORT_FOR_LOCAL_WEBSERVER/OPTIONAL
 }
 ```
 4. create a streamers.txt file in the config folder and add every streamer you want to poll on a new line, alternativly add the url to a txt file that contains the list to poll
